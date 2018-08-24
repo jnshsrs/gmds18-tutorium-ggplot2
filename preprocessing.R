a6e6dd21e224dd32df100e28f1ad32d2073ebf03
jnshsrs <jenshuesers@gmail.com>
2018-08-24 15:26:56 +0200

    initial commit, added slides, data, workbooks

---
+++ preprocessing.R
@@ -0,0 +1,29 @@
+library(gapminder)
+
+gapminder %>% 
+  mutate(lifeExp_cat = if_else(quantile(lifeExp, probs = .9) >= lifeExp, "High Life Expectency", "Low Life Expectancy")) %>% 
+  write_csv("data/gapminder.csv")
+
+titanic <- read_csv("~/Downloads/titanic.csv")
+titanic %>% 
+  mutate(
+    pclass = case_when(
+      pclass == 1 ~ "1st Class",
+      pclass == 2 ~ "2nd Class",
+      pclass == 3 ~ "3rd Class",
+      is.na(pclass) ~ as.character(NA),
+      TRUE ~ as.character(NA)
+    )
+  ) %>% 
+  mutate(
+    survived = if_else(survived == 1, "Yes", "No")
+  ) %>% 
+  mutate(
+    sex = case_when(
+      sex == "female" ~ "Female",
+      sex == "male" ~ "Male",
+      TRUE ~ as.character(NA)
+    )  
+  ) %>% 
+  write_csv("./data/titanic.csv")
+  
